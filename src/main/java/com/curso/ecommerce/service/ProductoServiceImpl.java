0d1925adb9ba54719288b5278d982c43a6fef372
calmahDev <caledmahatmacontrerasramos@gmail.com>
2025-04-15 00:32:09 -0500

    Implementacion de la funcionalidad para mostrar productos al administrador

--- src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
+++ src/main/java/com/curso/ecommerce/service/ProductoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,11 @@ public class ProductoServiceImpl implements ProductoService{
 	public void delete(Integer id) {
 		// TODO Auto-generated method stub
 		productoRepository.deleteById(id);
+	}
+
+	@Override
+	public List<Producto> findAll() {
+		return productoRepository.findAll();
 	}
 
 }
