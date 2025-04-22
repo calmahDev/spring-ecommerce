947fe98399c1fb694122329b6d93e27c3c8ebc11
calmahDev <caledmahatmacontrerasramos@gmail.com>
2025-04-22 04:23:21 -0500

    Mostrar la información en la vista resumenorden

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.curso.ecommerce.model.DetalleOrden;
 import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.IUsuarioService;
 import com.curso.ecommerce.service.ProductoService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,6 +30,10 @@ public class HomeController {
 	private final Logger  log=LoggerFactory.getLogger(HomeController.class);
 	@Autowired
 	private ProductoService productoService;
+	@Autowired
+	private IUsuarioService usuarioService;
+	
+	
 	//para almacenar los detalles de la orden
 	List<DetalleOrden> detalles = new ArrayList<DetalleOrden>();
 	//datos de la orden
@@ -117,7 +123,12 @@ public class HomeController {
 		return "/usuario/carrito";
 	}
 	@GetMapping("/order")
-	public String order() {
+	public String order(Model model) {
+		Usuario usuario = usuarioService.findById(1).get();
+		
+		model.addAttribute("cart",detalles);
+		model.addAttribute("orden",orden);
+		model.addAttribute("usuario",usuario);
 		return "usuario/resumenorden";
 	}
 	
--- src/main/java/com/curso/ecommerce/repository/IUsuarioRepository.java
+++ src/main/java/com/curso/ecommerce/repository/IUsuarioRepository.java
@@ -3,9 +3,8 @@ package com.curso.ecommerce.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.curso.ecommerce.model.Producto;
-
+import com.curso.ecommerce.model.Usuario;
 @Repository
-public interface ProductoRepository extends JpaRepository<Producto,Integer>{
-
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
+	
 }
