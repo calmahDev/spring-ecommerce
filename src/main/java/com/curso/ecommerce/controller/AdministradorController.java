1fb8c393032f606e767d749920d59efb9db47e7c
calmahDev <caledmahatmacontrerasramos@gmail.com>
2025-05-01 03:34:36 -0500

    Funcionalidad para ver las ordenes del sistema 46

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.service.IOrdenService;
 import com.curso.ecommerce.service.IUsuarioService;
 import com.curso.ecommerce.service.ProductoService;
 
@@ -19,6 +20,8 @@ public class AdministradorController {
 	private ProductoService  productoService ;
 	@Autowired
 	private IUsuarioService usuarioService;
+	@Autowired
+	private IOrdenService ordenService;
 	
 	@GetMapping("")
 	public String home (Model model) {
@@ -30,5 +33,10 @@ public class AdministradorController {
 	public String usuarios(Model model) {
 		model.addAttribute("usuarios",usuarioService.findAll());
 		return "administrador/usuarios";
+	}
+	@GetMapping("/ordenes")
+	public String ordenes(Model model) {
+		model.addAttribute("ordenes",ordenService.findAll());
+		return "administrador/ordenes";
 	}
 }
